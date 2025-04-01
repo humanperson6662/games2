484f6244ca6d3d7c5f4224284bdd6d6a8c78b8cb
ButterToasty <106360708+ButterToasty@users.noreply.github.com>
2025-04-01 22:54:55 +0000

    yurr

---
+++ murder is game over/gamereal/js/plugins/goldwindowremove.js
@@ -0,0 +1,5 @@
+(function() {
+Window_Gold.prototype.windowHeight = function() { return 0; };
+Window_Gold.prototype.windowWidth = function() { return 0; };
+Window_Gold.prototype.refresh = function() {};
+})();
---
+++ murder is game over/gamereal/js/plugins/master_volume.js
@@ -0,0 +1,4 @@
+AudioManager.bgmVolume = 50;
+AudioManager.bgsVolume = 20;
+AudioManager.meVolume = 60;
+AudioManager.seVolume = 75;
